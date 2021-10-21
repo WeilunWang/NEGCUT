2ff4c1cc490856dea4dd3dcf18d5b423f94bd9cd
Weilun Wang <250501715@qq.com>
2021-10-21 21:33:30 +0800

    Update unaligned_dataset.py

--- data/unaligned_dataset.py
+++ data/unaligned_dataset.py
@@ -61,10 +61,8 @@ class UnalignedDataset(BaseDataset):
         # Apply image transformation
         # For FastCUT mode, if in finetuning phase (learning rate is decaying),
         # do not perform resize-crop data augmentation of CycleGAN.
-#        print('current_epoch', self.current_epoch)
         is_finetuning = self.opt.isTrain and self.current_epoch > self.opt.n_epochs
         modified_opt = util.copyconf(self.opt, load_size=self.opt.crop_size if is_finetuning else self.opt.load_size)
-        # modified_opt = util.copyconf(self.opt, load_size=self.opt.load_size)
         transform = get_transform(modified_opt)
         A = transform(A_img)
         B = transform(B_img)
